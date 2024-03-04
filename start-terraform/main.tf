acd1d403b46df529d9ae48c1c4ebb734187edcc2
rachid-jl <rachid.jlassi2@gmail.com>
2024-03-04 09:40:25 +0100

    test 2

--- start-terraform/main.tf
+++ start-terraform/main.tf
@@ -16,9 +16,10 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "tutorial"
+  name  = var.my_container_test
   ports {
     internal = 80
     external = 8000
   }
 }
+
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "my_container_test" {
+  description = "this is à test container"
+  type = string
+  default = "nginx_container_test"
+}
